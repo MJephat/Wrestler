840a3bf31f3105207c0cf9f1167018af7383e795
MJephat <mukundijephat22@gmail.com>
2023-06-06 21:24:21 +0300

    created relationships

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class Wrestler(Base):
     age = Column(Integer())
     gender = Column(CHAR(1))
 
+    reviews = relationship('Review', backref=backref('wrestler'))
 
     def __repr__(self):
         return f'Wretler(id={self.id}, ' + \
@@ -33,6 +34,8 @@ class Stadium(Base):
     id = Column(Integer(), primary_key=True)
     Title = Column(String())
     country = Column(String())
+    championships = relationship('Championship', backref=backref('stadium'))
+
 
     def __repr__(self):
         return f'Stadium(id={self.id}, ' + \
@@ -46,7 +49,8 @@ class Championship(Base):
     id = Column(Integer(), primary_key=True)
     Title = Column(String())
     # make = Column(String())
-
+    matches = relationship('Match', backref=backref('championship'))
+    stadium_id = Column(Integer(), ForeignKey('stadiums.id'))
     def __repr__(self):
         return f'Championship(id={self.id}, ' + \
             f'Name={self.Title}, ' 
@@ -59,6 +63,8 @@ class  Match(Base):
     id = Column(Integer(), primary_key=True)
     category = Column(String())
     duration = Column(Integer())
+    championship_id = Column(Integer(), ForeignKey('championships.id'))
+    reviews = relationship('Review', backref=backref('match'))
 
     def __repr__(self):
         return f'Stadium(id={self.id}, ' + \
@@ -71,11 +77,15 @@ class Review(Base):
 
     id = Column(Integer(), primary_key=True)
     rating = Column(Integer())
+    match_id = Column(Integer(), ForeignKey('matches.id'))
+    wrestler_id = Column(Integer(), ForeignKey('wrestlers.id'))
     
     def __repr__(self):
         return f'Review(id={self.id}, ' + \
             f'rating={self.rating},' + \
             f'restaurant_id={self.restaurant_id})'
+       
+        
 
 
 if __name__ == '__main__':
@@ -103,23 +113,47 @@ if __name__ == '__main__':
     stadium5 = Stadium(Title = "Ahoy_Rotterdam", country = "Netherlands")
     stadium6 = Stadium(Title = "Barclays_Center", country = "USA")
 
-    match1 = Match(category = "ladder_match", duration = 45)
-    match2 = Match(category = "Tag_team_match", duration = 20)
-    match3 = Match(category = "Royal_Rumble", duration = 60)
-    match4 = Match(category = "Elimination_match", duration = 50)
-    match5 = Match(category = "Hell_in_cell", duration = 60)
-    match6 = Match(category = "Money_in_the_Bank", duration = 30)
+    match1 = Match(category = "ladder_match", duration = 45, championship_id = 3)
+    match2 = Match(category = "Tag_team_match", duration = 20, championship_id = 2)
+    match3 = Match(category = "Royal_Rumble", duration = 60, championship_id = 3)
+    match4 = Match(category = "Elimination_match", duration = 50, championship_id = 4)
+    match5 = Match(category = "Hell_in_cell", duration = 60, championship_id = 2)
+    match6 = Match(category = "Money_in_the_Bank", duration = 30, championship_id = 1)
 
-    championship1 = Championship(Title = "Heavyweight")
-    championship2 = Championship(Title = "Intercontinental")
-    championship3 = Championship(Title = "Universal")
-    championship4 = Championship(Title = "United States")
-    championship5 = Championship(Title = "Tag_team")
+    championship1 = Championship(Title = "Heavyweight", stadium_id = 1)
+    championship2 = Championship(Title = "Intercontinental", stadium_id = 4)
+    championship3 = Championship(Title = "Universal", stadium_id = 3)
+    championship4 = Championship(Title = "United States", stadium_id = 4)
+    championship5 = Championship(Title = "Tag_team", stadium_id = 5)
+
+    review1 =  Review(rating = 5,match_id =4, wrestler_id = 4)
+    review2 =  Review(rating = 4,match_id =1, wrestler_id = 3 )
+    review3 =  Review(rating = 4,match_id =2, wrestler_id = 6)
+    review4 =  Review(rating = 3,match_id =4, wrestler_id = 4)
+    review5 =  Review(rating = 5,match_id =3, wrestler_id = 10)
 
 
     session.add_all([wrestler1, wrestler2, wrestler3,wrestler4,wrestler5,wrestler6,wrestler7,wrestler8,wrestler9,wrestler10])
     session.add_all([stadium1,stadium1,stadium2,stadium3,stadium4,stadium5,stadium6])
     session.add_all([championship1,championship2,championship3,championship4,championship5])
     session.add_all([match1, match2, match3, match4, match5,match6])
+    session.add_all([review1,review2,review3,review4,review5])
     session.commit()
+
+def main():
+
+    choice = 0
+    while choice !=5:
+        print("Welcome to the SITE!")
+        print("1) All the wrestlers")
+        print("2)championship won by a perticular wrestler ")
+        print("3) average of the rating")
+        print("4) all Stadium")
+        print("5) Quit ")
+        choice = int(input())
+
+        # choice += 1 
+
+if __name__ == "__main__":
+    main()
 
